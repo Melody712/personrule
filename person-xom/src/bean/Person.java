747b9c8a829b04c9e38481a7bf982d45cddc37c0
fengyang <fengyang@xwfintech.net>
2019-05-20 17:11:24 +0800

    初始提交

---
+++ person-xom/src/bean/Person.java
@@ -0,0 +1,36 @@
+package bean;
+
+import java.util.Random;
+
+public class Person {
+
+	private int age = -1;
+	private String sex = "";
+	private String msg = "";
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public static void main(String[] args){
+		Random random=new Random();
+		int a=random.nextInt(100);
+		System.out.println(a);
+	}
+		
+	
+}
---
+++ person-xom/src/bean/Result.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class Result {
+	
+	private String info = "";
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public Result(String info) {
+		super();
+		this.info = info;
+	}
+
+	public Result() {
+		super();
+	}
+	
+	
+
+	
+}
